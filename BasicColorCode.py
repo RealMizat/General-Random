8b7be3852355dbfe97d152789a95a97bf11ebe1d
Mizat <55223715+rrxyyrrx@users.noreply.github.com>
2019-10-23 15:51:54 -0400

    Added comments and little mistake
    Comments for any person who comes across to hopefully add a little help, or a ton of help. I just would like to know how this works, and I know plenty of people know how to create a color swatch or palette for saving color schemes. Somewhat like swatches at the paint stores. That is my goal as of now, and newer to python, never have created my own class, without it being part of a question or a tutorial or guide. It would help me a lot to see how this could be done. 
    
    Please anyone feel free to lend me a hand, help me learn, as thats pretty much what this is. Also I have tons of things I would love to add as I learn. Color format converter, as well as a theme generator for popular IDE like atom, using their favorite color schemes they saved. It shouldn't be that hard in theory, I just need to learn more, crazy to think how much and long I have learned and can't even figure this out.

--- BasicColorCode.py
+++ BasicColorCode.py
@@ -8,12 +8,29 @@ def call_me():
     clr = colorchooser.askcolor(title="select color")
     return(clr)
 
+#need class for windows or frames that will take that color (sub-class)
+#anyone whom comes across this feel free to help I'm new trying to create 
+#Either frames or Canvas' that fill to the colors choosen and ability to save 
+#Those colors or an image with color numbers, so I need to pass the RGB values from the output 
+#witch is a tuple, or the last value 4rd value in HEX to create the color
+#The easiest way I think would be to draw a very thick line so it's a square or close to it
+#Grid is probbaly going to be used, at least with my logic it should work, however I am probably wrong
+
+#Please help if you come across this, still trying to graspe class' and methods and all the different types, syntax and rules
+#There is so much to them, and never have created my own yet, like i'm attempting here.
+
 root = Tk()
 
 
-
+I found another version with the __name__ = __main__ or a couple
+#how ever I do not fully understand that, even though I have read up on it quit a bit
+#I would not be able to put it into anything I come up with 100% on my own
+#   So for now would like to leave it out.
 
 button = Button(root, text="change color", command=call_me)
 button.pack()
 root.geometry("400x400")
-root.mainloop(C)
+root.mainloop()
+
+#if anyone comes across this and don't feel like helping, however know a guide or tutorial that will help
+#maybe a similar tk or even wx code, either with a guide or tutorial or not please let me know
